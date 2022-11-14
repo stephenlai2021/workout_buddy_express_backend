82663d40599fa3dca1a3c157a63566e3002d8c8b
Stephen_Lai <stephenlai2021@gmail.com>
2022-11-14 22:50:22 +0800

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -30,14 +30,13 @@ userSchema.statics.signup = async function (email, password) {
   }
 
   const exists = await this.findOne({ email })
-
   if (exists) {
     throw Error('Email already in use')
   }
 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
-
+  
   const user = await this.create({ email, password: hash })
   return user
 }
@@ -48,13 +47,11 @@ userSchema.statics.login = async function(email, password) {
   }
 
   const user = await this.findOne({ email })
-
   if (!user) {
     throw Error('Incorrect email')
   }
 
   const match = await bcrypt.compare(password, user.password)
-
   if (!match) {
     throw Error('Incorrect password')
   }
